5e57b37c28259221a4fdde4fadf8d94f3ad2d48e
erfandavoodinasr <davoodinasr.erfan@gmail.com>
2025-01-27 17:23:22 +0330

    print Hello Dotin

--- src/main/java/ir/dotin/Main.java
+++ src/main/java/ir/dotin/Main.java
@@ -2,6 +2,7 @@ package ir.dotin;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Hello Dotin");
+        System.out.print("My name is Erfan Davoodi Nasr");
     }
 }
