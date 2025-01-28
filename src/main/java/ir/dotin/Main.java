1f5c33e97cd8cd5f3ab69f8c45361b5a6062b960
erfandavoodinasr <davoodinasr.erfan@gmail.com>
2025-01-28 14:19:32 +0330

    variable part 1
    - int for number

--- src/main/java/ir/dotin/Main.java
+++ src/main/java/ir/dotin/Main.java
@@ -2,7 +2,9 @@ package ir.dotin;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Dotin");
-        System.out.print("My name is Erfan Davoodi Nasr");
+        int height = 50;
+        System.out.println("When i was born my height was: " + height + "cm");
+        height = 180;
+        System.out.println("And today my height is: " + height + "cm");
     }
 }
