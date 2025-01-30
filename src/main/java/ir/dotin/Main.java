2c1305cbe1256c2f7059b4ceb0147b07f1d34256
erfandavoodinasr <davoodinasr.erfan@gmail.com>
2025-01-30 22:53:40 +0330

    String
    - initial String
    - concat
    - get length
    - get char

--- src/main/java/ir/dotin/Main.java
+++ src/main/java/ir/dotin/Main.java
@@ -3,11 +3,16 @@ package ir.dotin;
 
 public class Main {
     public static void main(String[] args) {
-        double temperature;
-        temperature = 36.6;
-        System.out.println("Temperature is: " + temperature + "°C");
-        boolean isRaining;
-        isRaining = false;
-        System.out.println("IS raining in Tehran? " + isRaining);
+        String firstName = "Erfan ";
+        String lastName = "Davoodi Nasr";
+
+        //String fullName = firstName.concat(lastName); // concat firstName and lastName
+        String fullName = firstName + lastName; // concat firstName and lastName
+
+        //System.out.println("My name is: ".concat(fullName)); // print full name
+        System.out.println("My name is: " + fullName); // print full name
+        System.out.println("Length of my name is: " + fullName.length() + "characters"); // get fullName length
+        System.out.println("My first name start with: " + fullName.charAt(0)); // first char of my full name
+        System.out.println("My first name end with: " + fullName.charAt(fullName.length()-1)); // last char of my full name
     }
 }
