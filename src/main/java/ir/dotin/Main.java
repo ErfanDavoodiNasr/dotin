99c71417a16b23bce7032300aa3761048cf209e8
erfandavoodinasr <davoodinasr.erfan@gmail.com>
2025-01-31 21:56:24 +0330

    Scanner
    - get input
    - init const

--- src/main/java/ir/dotin/Main.java
+++ src/main/java/ir/dotin/Main.java
@@ -1,18 +1,19 @@
 package ir.dotin;
 
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String firstName = "Erfan ";
-        String lastName = "Davoodi Nasr";
+        Scanner scanner = new Scanner(System.in);
+        final double TAX_RATE = 0.1; // for example tax rate is 10%
 
-        //String fullName = firstName.concat(lastName); // concat firstName and lastName
-        String fullName = firstName + lastName; // concat firstName and lastName
-
-        //System.out.println("My name is: ".concat(fullName)); // print full name
-        System.out.println("My name is: " + fullName); // print full name
-        System.out.println("Length of my name is: " + fullName.length() + "characters"); // get fullName length
-        System.out.println("My first name start with: " + fullName.charAt(0)); // first char of my full name
-        System.out.println("My first name end with: " + fullName.charAt(fullName.length()-1)); // last char of my full name
+        System.out.print("enter your income: ");
+        double income = scanner.nextDouble(); // get input income from user
+        double incomeTax = income * TAX_RATE; // calculate income tax
+        System.out.println("income tax for " + income + "$ is: " + incomeTax);
     }
 }
+
+
+
