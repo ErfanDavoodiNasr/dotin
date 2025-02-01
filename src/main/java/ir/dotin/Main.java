c8ade66ae049c2e42045060dcabec6a1fca6a03c
erfandavoodinasr <davoodinasr.erfan@gmail.com>
2025-02-02 00:05:22 +0330

    operators
    > < >= <= == !=

--- src/main/java/ir/dotin/Main.java
+++ src/main/java/ir/dotin/Main.java
@@ -4,13 +4,15 @@ public class Main {
     public static void main(String[] args) {
         double a = 10.0;
         double b = 25.5;
+        double c = 10.5;
         System.out.printf("""
-                a+b= %s
-                a-b= %s
-                a*b= %s
-                a/b= %s
-                a٪b= %s
-                """, a + b, a - b, a * b, a / b, a % b);
+                a > b : %s
+                a < c : %s
+                a <= c + b : %s
+                b != c + a : %s
+                b >= a + c : %s
+                a == c : %s
+                """, a > b, a < c, a <= c + b, b != c + a, b >= a + c, a == c);
 
     }
 }
