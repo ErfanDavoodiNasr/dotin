0cf64fbb6733f2a947954f424d03f3dce4051460
erfandavoodinasr <davoodinasr.erfan@gmail.com>
2025-02-01 23:56:28 +0330

    math
    - + / % *

--- src/main/java/ir/dotin/Main.java
+++ src/main/java/ir/dotin/Main.java
@@ -1,17 +1,17 @@
 package ir.dotin;
 
-
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        final double TAX_RATE = 0.1; // for example tax rate is 10%
+        double a = 10.0;
+        double b = 25.5;
+        System.out.printf("""
+                a+b= %s
+                a-b= %s
+                a*b= %s
+                a/b= %s
+                a٪b= %s
+                """, a + b, a - b, a * b, a / b, a % b);
 
-        System.out.print("enter your income: ");
-        double income = scanner.nextDouble(); // get input income from user
-        double incomeTax = income * TAX_RATE; // calculate income tax
-        System.out.println("income tax for " + income + "$ is: " + incomeTax);
     }
 }
 
