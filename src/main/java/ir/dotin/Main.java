73402527d46cd674eab17a0244c122f0531b341f
erfandavoodinasr <davoodinasr.erfan@gmail.com>
2025-02-03 23:42:30 +0330

    increment and decrement operators

--- src/main/java/ir/dotin/Main.java
+++ src/main/java/ir/dotin/Main.java
@@ -2,18 +2,13 @@ package ir.dotin;
 
 public class Main {
     public static void main(String[] args) {
-        double a = 10.0;
-        double b = 25.5;
-        double c = 10.5;
-        System.out.printf("""
-                a > b : %s
-                a < c : %s
-                a <= c + b : %s
-                b != c + a : %s
-                b >= a + c : %s
-                a == c : %s
-                """, a > b, a < c, a <= c + b, b != c + a, b >= a + c, a == c);
-
+        int x = 0;
+        x++;
+        x++;
+        x++;
+        --x;
+        --x;
+        System.out.println("final result is: " + x);
     }
 }
 
