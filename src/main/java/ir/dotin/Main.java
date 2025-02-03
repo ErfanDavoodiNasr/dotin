9423aa3b029142b3f282111aa0ca0a8e79424a1d
erfandavoodinasr <davoodinasr.erfan@gmail.com>
2025-02-03 23:52:38 +0330

    find odd and even numbers
    - if statements

--- src/main/java/ir/dotin/Main.java
+++ src/main/java/ir/dotin/Main.java
@@ -1,14 +1,19 @@
 package ir.dotin;
 
+import java.util.Scanner;
+
 public class Main {
+    private final static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        int x = 0;
-        x++;
-        x++;
-        x++;
-        --x;
-        --x;
-        System.out.println("final result is: " + x);
+        System.out.print("enter a number: ");
+        long enteredNumber = scanner.nextLong(); // get input number
+
+        if ((enteredNumber % 2) == 0) {
+            System.out.println(enteredNumber + " is even");
+        } else {
+            System.out.println(enteredNumber + " is odd");
+        }
     }
 }
 
