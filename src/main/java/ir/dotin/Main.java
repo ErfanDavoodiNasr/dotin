77862a0f8c91ed203e39db1f7c04c7948fe27f23
erfandavoodinasr <davoodinasr.erfan@gmail.com>
2025-02-04 22:24:31 +0330

    practise

--- src/main/java/ir/dotin/Main.java
+++ src/main/java/ir/dotin/Main.java
@@ -6,13 +6,18 @@ public class Main {
     private final static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.print("enter a number: ");
-        long enteredNumber = scanner.nextLong(); // get input number
+        System.out.print("enter first number: ");
+        long firstNumber = scanner.nextLong(); // get input number
 
-        if ((enteredNumber % 2) == 0) {
-            System.out.println(enteredNumber + " is even");
-        } else {
-            System.out.println(enteredNumber + " is odd");
+        System.out.print("enter second number: ");
+        long secondNumber = scanner.nextLong(); // get input number
+
+        if (firstNumber > secondNumber){
+            System.out.println("first number is greater than second number");
+        }else if (secondNumber > firstNumber){
+            System.out.println("second number is greater than first number");
+        }else {
+            System.out.println("both numbers are equal");
         }
     }
 }
